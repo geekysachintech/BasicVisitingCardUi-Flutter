9d22d288ec33cf62b6a99c9db27c67f8c34468a0
geekysachintech <geekysachin.info@gmail.com>
2020-06-26 20:29:48 +0530

    first commit

---
+++ mi_card_flutter-master/lib/main.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/sachin.jpg'),
+              ),
+              Text(
+                'Sachin Kr',
+                style: TextStyle(
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Pacifico',
+                ),
+              ),
+              Text(
+                'ANDROID DEVELOPER',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.teal.shade100,
+                  letterSpacing: 2.5,
+                  fontFamily: 'Source Sans Pro',
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 20,
+              width: 150.0,
+              child: Divider(
+                color: Colors.teal.shade100
+              ),),
+              Card(
+                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25),
+                  child: ListTile(
+                    leading: Icon(Icons.email, color: Colors.teal),
+                    title: Text(
+                      '+91 987 654 3210',
+                      style: TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.teal.shade900,
+                          fontFamily: 'Source Sans Pro'),
+                    ),
+                  )),
+              Card(
+                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25),
+                  child: ListTile(
+                    leading: Icon(Icons.email, color: Colors.teal),
+                    title: Text(
+                      'sachin@gmail.com',
+                      style: TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.teal.shade900,
+                          fontFamily: 'Source Sans Pro'),
+                    ),
+                  )),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
